349dc428ec6f347789949509284a0b0e06578cea
Dave <dave.schwantes@locusenergy.com>
2012-08-16 14:20:01 -0700

    ready for Heroku

---
+++ auth_account.rb
@@ -0,0 +1,14 @@
+class AuthAccount
+	def initialize
+		@username = "RunningAPIBot" # set auth account username
+		@password = "cgxc02" # set auth account password
+	end
+
+	def username
+		return @username
+	end
+
+	def password
+		return @password
+	end
+end
---
+++ config.ru
@@ -0,0 +1,2 @@
+require './fitocracy_runs_api'
+run Sinatra::Application
